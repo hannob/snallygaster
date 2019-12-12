18ccd9959a0d4528106560853f94f36b1859be38
Hanno Böck <hanno@gentoo.org>
2019-12-12 18:06:06 +0100

    raise version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'),
 
 setuptools.setup(
     name=package_name,
-    version="0.0.2",
+    version="0.0.3",
     description="Tool to scan for secret files on HTTP servers",
     long_description=readme,
     long_description_content_type='text/markdown',

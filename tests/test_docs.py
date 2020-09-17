c41bfddea3a9aeea9e922416ed611281fac3fd46
Hanno Böck <hanno@gentoo.org>
2020-09-17 14:57:07 +0200

    check if we document all the tests in TESTS.md

---
+++ tests/test_docs.py
@@ -0,0 +1,23 @@
+import unittest
+import re
+
+
+class TestCodingstyle(unittest.TestCase):
+    # checking if there is documentation for all snallygaster tests
+    # in the TEST.md documentation
+    def test_docs(self):
+        f = open("snallygaster", "r")
+        funcs = re.findall("def test_([a-z_]*)", f.read())
+        f.close()
+        fd = open("TESTS.md", "r")
+        docs = []
+        for line in fd.readlines():
+            if line.startswith("---"):
+                docs.append(ol.rstrip())
+            ol = line
+        fd.close()
+        self.assertEqual(funcs, docs)
+
+
+if __name__ == '__main__':
+    unittest.main()

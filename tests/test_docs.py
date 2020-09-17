d6cf24f0212bd30c27a9dc65c13f8b42fbcdb3fb
Hanno Böck <hanno@gentoo.org>
2020-09-17 15:02:15 +0200

    fix pyflakes and pyupgrade warnings

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -6,11 +6,12 @@ class TestCodingstyle(unittest.TestCase):
     # checking if there is documentation for all snallygaster tests
     # in the TEST.md documentation
     def test_docs(self):
-        f = open("snallygaster", "r")
+        f = open("snallygaster")
         funcs = re.findall("def test_([a-z_]*)", f.read())
         f.close()
-        fd = open("TESTS.md", "r")
+        fd = open("TESTS.md")
         docs = []
+        ol = ""
         for line in fd.readlines():
             if line.startswith("---"):
                 docs.append(ol.rstrip())

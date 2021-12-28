8e076ee8d9239f173be4cf581c3a78f2a5c3a7e1
Hanno Böck <hanno@gentoo.org>
2021-12-28 16:43:14 +0100

    raise version to 0.0.11

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ f.close()
 
 setuptools.setup(
     name=package_name,
-    version="0.0.10",
+    version="0.0.11",
     description="Tool to scan for secret files on HTTP servers",
     long_description=readme,
     long_description_content_type='text/markdown',

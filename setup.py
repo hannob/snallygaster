3179b7ae902b779eab02e67eb937007e39c9d495
Hanno Böck <hanno@gentoo.org>
2022-11-16 17:33:40 +0100

    reformat setup.py with black

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@
 import os
 import setuptools
 
-package_name = 'snallygaster'
+package_name = "snallygaster"
 
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+with open(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md"),
+    encoding="utf-8",
+) as f:
     readme = f.read()
 
 setuptools.setup(
@@ -13,31 +16,29 @@ setuptools.setup(
     version="0.0.12",
     description="Tool to scan for secret files on HTTP servers",
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author="Hanno Böck",
-    author_email='hanno@hboeck.de',
-    url='https://github.com/hannob/snallygaster',
+    author_email="hanno@hboeck.de",
+    url="https://github.com/hannob/snallygaster",
     packages=[],
-    scripts=['snallygaster'],
-    python_requires='>=3',
-    install_requires=[
-        'urllib3',
-        'beautifulsoup4',
-        'dnspython'
-    ],
+    scripts=["snallygaster"],
+    python_requires=">=3.7",
+    install_requires=["urllib3", "beautifulsoup4", "dnspython"],
     license="CC0",
     zip_safe=True,
-    keywords=['security', 'vulnerability', 'http'],
+    keywords=["security", "vulnerability", "http"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: System Administrators',
-        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: System Administrators",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
 )
